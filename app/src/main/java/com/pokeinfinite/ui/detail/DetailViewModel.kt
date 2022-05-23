52f7cfb4c633dc94fd378401347ae93fc9ff1279
Joshua Jeremia <79564114+equinox-jj@users.noreply.github.com>
2022-05-24 00:43:03 +0700

    Add Detail Pokemon.

--- app/src/main/java/com/pokeinfinite/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/pokeinfinite/ui/detail/DetailViewModel.kt
@@ -1,12 +1,40 @@
 package com.pokeinfinite.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pokeinfinite.data.ApiResource
+import com.pokeinfinite.data.model.SinglePokemonResponse
+import com.pokeinfinite.data.repository.PokemonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-
+    private val repository: PokemonRepository
 ) : ViewModel() {
+
+    private val _pokemonDetailResponse = MutableLiveData<ApiResource<SinglePokemonResponse>>()
+    val pokemonDetailResponse: LiveData<ApiResource<SinglePokemonResponse>> get() = _pokemonDetailResponse
+
+    fun getPokemonPagingSource(queryName: String) {
+        viewModelScope.launch {
+            repository.getPokemonDetail(queryName)
+                .onStart {
+
+                }
+                .catch {
+
+                }
+                .collect {
+                    _pokemonDetailResponse.value = it
+                }
+        }
+    }
 
 }
