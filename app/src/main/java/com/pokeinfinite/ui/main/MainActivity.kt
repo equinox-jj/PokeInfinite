97b51ad8151d44e0fb2b548bde2c996d14d04683
Joshua Jeremia <79564114+equinox-jj@users.noreply.github.com>
2022-05-29 17:36:49 +0700

    Add Apk and Splash Screen.

--- app/src/main/java/com/pokeinfinite/ui/main/MainActivity.kt
+++ app/src/main/java/com/pokeinfinite/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.pokeinfinite.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -18,8 +20,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var navHostFragment: NavHostFragment
 
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { viewModel.isLoading.value }
+        }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
---
+++ app/src/main/java/com/pokeinfinite/ui/main/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.pokeinfinite.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(1000)
+            _isLoading.value = false
+        }
+    }
+
+}
